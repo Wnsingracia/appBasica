2acf14452764c818ee91f14f718deb6f4e4a8957
gabriCt <samigavincha16@gmail.com>
2025-12-09 01:10:34 -0400

    Add comments for clarity in main.dart
    Added comments to clarify the purpose of the main app and its components.

--- appbasica/lib/main.dart
+++ appbasica/lib/main.dart
@@ -1,15 +1,24 @@
-import 'package:appbasica/app.dart';
+import 'package:appbasica/app.dart'; // Importa la pantalla principal de la app
 import 'package:flutter/material.dart';
 
+/// Punto de entrada de la app
 void main() {
-  runApp(const MainApp());
+  runApp(const MainApp()); // Ejecuta la aplicación
 }
 
+/// Widget principal de la app
+/// Contiene un [MaterialApp] que es el contenedor principal de Flutter
+/// y carga la [PantallaPrincipal] dentro de un Scaffold.
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: PantallaPrincipal()));
+    return MaterialApp(
+      // [home] define la primera pantalla que se mostrará al abrir la app
+      home: Scaffold(
+        body: PantallaPrincipal(), // Carga la pantalla principal de la app
+      ),
+    );
   }
 }
